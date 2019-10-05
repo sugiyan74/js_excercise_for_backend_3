b17ce69743e327bc4a9dfe6cf24a513c7b5e9695
ksuigyama <k.sugiyama@apassist.com>
2019-10-05 18:11:00 +0900

    Created the answer

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+class Counter {
+    constructor() {
+        this._count = 0;
+    }
+    increment() {
+        this._count++;
+    }
+    decrement() {
+        this._count--;
+    }
+}
+
+module.exports = Counter;
