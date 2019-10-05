312bd4cee6a9fd82fb47f0b974d379f9f94cbd0f
ksuigyama <k.sugiyama@apassist.com>
2019-10-05 20:04:51 +0900

    fix files

--- test/src/Counter.spec.js
+++ test/src/Counter.spec.js
@@ -1,39 +1,39 @@
-const assert = require(power-assert);
-const Counter = require(../../src/Counter);
+const assert = require('power-assert');
+const Counter = require('../../src/Counter');
 
 describe('Counter.jsテスト', () => {
     it('_countプロパティを持つ', () => {
-       const counter = New Counter();
-       assert.equal(counter.hasProperty('_count'), true); 
+       const counter = new Counter();
+       assert.equal(counter.hasOwnProperty('_count'), true);
     });
 
     it('_countプロパティの初期値は0である', () => {
-        const counter = New Counter();
-        assert.equal(couter._couter, 0);
+        const counter = new Counter();
+        assert.equal(counter._count, 0);
     });
 
     it('incrementメソッドを持つ', () => {
-        const counter = New Counter();
+        const counter = new Counter();
         assert.equal(typeof counter.increment === 'function', true);
     });
 
     it('incrementメソッドを実行すると_countプロパティの値は1増える', () => {
-        const counter = New Counter();
-        assert.equal(counter._couter, 0);
+        const counter = new Counter();
+        assert.equal(counter._count, 0);
         counter.increment();
-        assert.equal(counter._couter, 1);
+        assert.equal(counter._count, 1);
     });
 
     it('decrementメソッドを持つ', () => {
-        const counter = New Counter();
+        const counter = new Counter();
         assert.equal(typeof counter.decrement === 'function', true);
     });
 
     it('decrementメソッドを実行すると_countプロパティの値は1減る', () => {
-        const counter = New Counter();
-        assert.equal(counter._couter, 0);
+        const counter = new Counter();
+        assert.equal(counter._count, 0);
         counter.decrement();
-        assert.equal(counter._couter, -1);
+        assert.equal(counter._count, -1);
     });
 });
 
